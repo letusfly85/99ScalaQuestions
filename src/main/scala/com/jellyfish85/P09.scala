dc60c8aff0dd798fff0cf4051411113a2981b127
letusfly85 <letusfly95@gamil.com>
2015-01-23 22:29:11 +0900

    add P09

---
+++ src/main/scala/com/jellyfish85/P09.scala
@@ -0,0 +1,34 @@
+object P09 {
+
+    def main(args: Array[String]) {
+        val list = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+        println(list.drop(1))
+        println(pack(list))
+    }
+
+    /*
+    def pack[T](list: List[T]):List[(Int, T)] = {
+        def recur(elem: T, acc: List[(Int, T)]): List[(Int, T)] = {
+            acc match {
+                case x :: xs if (elem == x._2) => (x._1 + 1, elem) :: acc.drop(1)
+                case x :: xs => (1, elem) :: acc
+                case Nil => List((1, elem))
+            }
+        }
+
+        var initList: List[(Int, T)] = List()
+        list.foldRight(initList)((elem, acc) => recur(elem, acc))
+    }
+    */
+    def pack[T](list: List[T]):List[List[T]] = {
+        def recur(elem: T, acc: List[List[T]]): List[List[T]] = {
+            acc match {
+                case x :: xs if (elem == x(0)) => (elem::x) :: acc.drop(1)
+                case _ => List(elem) :: acc
+            }
+        }
+
+        var initList: List[List[T]] = List()
+        list.foldRight(initList)((elem, acc) => recur(elem, acc))
+    }
+}
