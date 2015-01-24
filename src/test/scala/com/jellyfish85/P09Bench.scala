b4bbeb72180ace2a321e7d89268eab0be644fc9d
shunsuke wada <letusfly95@gmail.com>
2015-01-24 20:44:36 +0900

    測定範囲変更

--- src/test/scala/com/jellyfish85/P09Bench.scala
+++ src/test/scala/com/jellyfish85/P09Bench.scala
@@ -5,7 +5,7 @@ import org.scalameter.api._
 class P09Bench extends PerformanceTest.Microbenchmark {
    val p09 = new P09()
 
-   val sizes = Gen.range("size")(0, 10000, 1000)
+   val sizes = Gen.range("size")(0, 100000, 10000)
    val lists = for (sz <- sizes) yield (0 until sz).toList
    measure method "pack" in {
        using(lists) in {
